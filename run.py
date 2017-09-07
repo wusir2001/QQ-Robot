8ffc4237604bfe0020083cc847f2ceb2daf854cb
iceCory <icecory520@gmail.com>
2017-09-07 09:38:04 +0800

    修改run

--- run.py
+++ run.py
@@ -5,39 +5,68 @@
 # @Link    : http://www.bitwater1997.cn
 # @Version : $Id$
 
-import os
+# coding: utf-8
 import sys
-import time
+import os
 
-# 产生子进程，而后父进程退出
-pid = os.fork()
-if pid > 0:
-    sys.exit(0)
 
-# 修改子进程工作目录
-os.chdir("/")
-# 创建新的会话，子进程成为会话的首进程
-os.setsid()
-# 修改工作目录的umask
-os.umask(0)
+'''
+    将当前进程fork为一个守护进程
+    注意：如果你的守护进程是由inetd启动的，不要这样做！inetd完成了
+    所有需要做的事情，包括重定向标准文件描述符，需要做的事情只有chdir()和umask()了
 
-# 创建孙子进程，而后子进程退出
-pid = os.fork()
-if pid > 0:
-    sys.exit(0)
+'''
 
-# 重定向标准输入流、标准输出流、标准错误
-sys.stdout.flush()
-sys.stderr.flush()
-si = open("/dev/null", 'r')
-so = open("/dev/null", 'a+')
-se = open("/dev/null", 'a+', 0)
-os.dup2(si.fileno(), sys.stdin.fileno())
-os.dup2(so.fileno(), sys.stdout.fileno())
-os.dup2(se.fileno(), sys.stderr.fileno())
+
+def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+    # 重定向标准文件描述符（默认情况下定向到/dev/null）
+    try:
+        pid = os.fork()
+        # 父进程(会话组头领进程)退出，这意味着一个非会话组头领进程永远不能重新获得控制终端。
+        if pid > 0:
+            sys.exit(0)  # 父进程退出
+    except OSError as e:
+        sys.stderr.write("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror))
+        sys.exit(1)
+
+    # 从母体环境脱离
+    # chdir确认进程不保持任何目录于使用状态，否则不能umount一个文件系统。也可以改变到对于守护程序运行重要的文件所在目录
+    os.chdir("/")
+    os.umask(0)  # 调用umask(0)以便拥有对于写的任何东西的完全控制，因为有时不知道继承了什么样的umask。
+    os.setsid()  # setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离。
+
+    # 执行第二次fork
+    try:
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)  # 第二个父进程退出
+    except OSError as e:
+        sys.stderr.write("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror))
+        sys.exit(1)
+
+    # 进程已经是守护进程了，重定向标准文件描述符
+
+    for f in sys.stdout, sys.stderr:
+        f.flush()
+    si = open(stdin, 'r')
+    so = open(stdout, 'a+')
+    se = open(stderr, 'a+b', 0)
+    os.dup2(si.fileno(), sys.stdin.fileno())  # dup2函数原子化关闭和复制文件描述符
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())
+
+# 示例函数：每秒打印一个数字和时间戳
 
 
 from SmartQQ import QMcore
 from config import APIkey
-qm = QMcore(APIkey)
-qm.start()
+
+
+def main():
+    qm = QMcore(APIkey)
+    qm.start()
+
+
+if __name__ == "__main__":
+    daemonize('/dev/null', '/tmp/daemon_stdout.log', '/tmp/daemon_error.log')
+    main()
