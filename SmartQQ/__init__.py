61688202d995539fd4b7c12a945e0b539935ede4
iceCory <icecory520@gmail.com>
2017-12-09 20:20:41 +0800

    删除旧架构的文件

--- SmartQQ/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2017-07-07 09:27:01
-# @Author  : bitwater (bitwater1997@gmail.com)
-# @Link    : http://www.bitwater1997.cn
-# @Version : 1.0
-
-from .QMcore import QMcore
-
